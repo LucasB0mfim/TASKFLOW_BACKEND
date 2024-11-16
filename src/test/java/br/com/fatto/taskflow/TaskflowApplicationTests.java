730148489a3620d36f9fd6d4e8431f730d55babf
lucas <lucasbomfimsobral@gmail.com>
2024-11-16 07:45:00 -0300

    first commit

---
+++ src/test/java/br/com/fatto/taskflow/TaskflowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatto.taskflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
